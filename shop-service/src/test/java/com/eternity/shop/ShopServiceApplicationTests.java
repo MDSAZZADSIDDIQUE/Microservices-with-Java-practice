8044ab9ce327ad4f7948fa46337ef94739e2ebe1
MDSAZZADSIDDIQUE <mdsazzadsiddique0@gmail.com>
2024-08-08 13:53:50 +0600

    Create product unit test add

--- shop-service/src/test/java/com/eternity/shop/ShopServiceApplicationTests.java
+++ shop-service/src/test/java/com/eternity/shop/ShopServiceApplicationTests.java
@@ -1,15 +1,45 @@
 package com.eternity.shop;
 
+import io.restassured.RestAssured;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.shaded.org.hamcrest.Matchers;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ShopServiceApplicationTests {
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
 
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+				"name": "Plant",
+				"description": "It's a plant",
+				"price": 1000
+				}
+				""";
+		RestAssured.given().contentType("application/json").body(requestBody).when().post("/api/product").then().statusCode(201);
 	}
 
 }

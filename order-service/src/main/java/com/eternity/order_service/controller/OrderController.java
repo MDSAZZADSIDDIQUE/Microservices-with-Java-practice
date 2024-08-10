68e08aca6f630abc8f234870db9d5bf30ca1a736
MDSAZZADSIDDIQUE <mdsazzadsiddique0@gmail.com>
2024-08-10 23:13:36 +0600

    Flyway add

---
+++ order-service/src/main/java/com/eternity/order_service/controller/OrderController.java
@@ -0,0 +1,26 @@
+package com.eternity.order_service.controller;
+
+import com.eternity.order_service.dto.OrderRequest;
+import com.eternity.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+}

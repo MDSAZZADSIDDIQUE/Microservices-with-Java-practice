caf8fb392e088d2ab4bf775fd2696722644f362f
Md Sazzad Siddique <mdsazzadsiddique0@gmail.com>
2024-08-24 19:48:29 +0600

    Open feign to rest client convert

--- order-service/src/main/java/com/eternity/order_service/client/InventoryClient.java
+++ order-service/src/main/java/com/eternity/order_service/client/InventoryClient.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
-
 }

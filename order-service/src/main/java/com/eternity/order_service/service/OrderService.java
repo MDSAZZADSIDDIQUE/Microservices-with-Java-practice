16646fdb0b3b851f4185b4c4babe86b69a80fc0b
Md Sazzad Siddique <mdsazzadsiddique0@gmail.com>
2024-08-25 14:39:28 +0600

    Apache Kafka add

--- order-service/src/main/java/com/eternity/order_service/service/OrderService.java
+++ order-service/src/main/java/com/eternity/order_service/service/OrderService.java
@@ -2,12 +2,13 @@ package com.eternity.order_service.service;
 
 import com.eternity.order_service.client.InventoryClient;
 import com.eternity.order_service.dto.OrderRequest;
+import com.eternity.order_service.event.OrderPlacedEvent;
 import com.eternity.order_service.model.Order;
 import com.eternity.order_service.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
 import java.util.UUID;
 
@@ -17,18 +18,30 @@ import java.util.UUID;
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
+
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
         if (isProductInStock) {
             Order order = new Order();
             order.setOrderNumber(UUID.randomUUID().toString());
-            order.setPrice(orderRequest.price());
+            order.setPrice(orderRequest.price().multiply(BigDecimal.valueOf(orderRequest.quantity())));
             order.setSkuCode(orderRequest.skuCode());
             order.setQuantity(orderRequest.quantity());
             orderRepository.save(order);
+
+            // Send the message to Kafka Topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
-            throw new RuntimeException("Product with SkuCode is not in stock");
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
         }
     }
 }
